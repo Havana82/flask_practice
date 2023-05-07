1d1dba3302a132efe64c2716417ee15ceb1e5a63
halimAremu <halimat3000@gmail.com>
2023-05-07 09:31:43 -0500

    deployment

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import db
 from app.models.book import Book
+from app.models.author import Author
 from flask import Blueprint, jsonify, abort, make_response, request
 
 # cnalass Book:
@@ -14,50 +15,48 @@ from flask import Blueprint, jsonify, abort, make_response, request
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 # helper function
-def validate_book(book_id):
+authors_bp = Blueprint("authors_bp", __name__, url_prefix="/authors")
+def validate_model(cls, model_id):
     try:
-        book_id = int(book_id)
+        model_id = int(model_id)
     except:
-        abort(make_response({"message":f"book {book_id} invalid"}, 400))
-    book = Book.query.get(book_id)
-    if not book :
-        abort(make_response({"message": f"book {book_id} not found"}, 404))
-    return book
+        abort(make_response({"message":f"{cls.__name__} {model_id} invalid"}, 400))
+    model = cls.query.get(model_id)
+    if not model :
+        abort(make_response({"message" : f"{cls.__name__} {model_id} not found"}, 404))
+    return model
 
 # routes
 @books_bp.route("", methods=['POST'])
 def create_book():
     request_body = request.get_json()
-    new_book = Book(title=request_body["title"],description= request_body["description"] )
+    new_book = Book.from_dict(request_body)
     db.session.add(new_book)
     db.session.commit()
+    
     return make_response(jsonify(f"Book {new_book.title} was created successfully"), 201)
 
 @books_bp.route("", methods=["GET"])
 def read_all_books():
+    title_query = request.args.get("title")
+    if title_query:
+        books = Book.query.filter_by(title=title_query)
+    else:
+        books = Book.query.all()
     books_response= []
-    books = Book.query.all()
     for book in books:
-        books_response.append({
-            "id": book.id,
-            "title": book.title,
-            "description": book.description
-        })
+        books_response.append(book.to_dict())
     return jsonify(books_response)
 
 @books_bp.route("/<book_id>", methods=["GET"])
 def read_one_book(book_id):
-    book = validate_book(book_id)
+    book = validate_model(Book, book_id)
     
-    return{
-            "id": book.id,
-            "title": book.title,
-            "description": book.description
-            } 
+    return book.to_dict()
 
 @books_bp.route("/<book_id>", methods = ["PUT"])
 def update_book(book_id):
-    book = validate_book(book_id)
+    book = validate_model(Book, book_id)
     request_body = request.get_json()
     book.title = request_body["title"]
     book.description = request_body["description"]
@@ -67,7 +66,50 @@ def update_book(book_id):
 
 @books_bp.route("/<book_id>", methods = ["DELETE"])
 def delete_book(book_id):
-    book = validate_book(book_id)
+    book = validate_model(Book, book_id)
     db.session.delete(book)
     db.session.commit()
     return make_response(jsonify(f"Book {book_id} deleted succesffuly"))
+
+@authors_bp.route("", methods = ['POST'])
+def create_author():
+    request_body = request.get_json()
+    new_author = Author(name = request_body["name"])
+    
+    db.session.add(new_author)
+    db.session.commit
+    
+@authors_bp.route("", methods = ['GET'])
+def get_authors():
+    response_body = []
+    authors = Author.query.all()
+    for author in authors:
+        response_body.append({"name": author.name})
+    
+    return jsonify(response_body)
+
+@authors_bp.route('/<author_id>/books', methods = ['POST'])
+def create_book(author_id):
+    author = validate_model(Author, author_id)
+    request_body = request.get_json()
+    new_book = Book(title=request_body["title"],
+                    description=request_body["description"],
+                    author=author)
+    db.session.add(new_book)
+    db.session.commit()
+    return make_response(jsonify(f"Book {new_book.title} by {new_book.author.name} successfully created"), 201)
+
+@authors_bp.route('/<author_id>/books', methods=['GET'])
+def read_books(author_id):
+    author = validate_model(Author, author_id)
+    books_response = []
+    for book in author.books:
+        books_response.append(
+            {"id": book.id,
+            "title": book.title,
+            "description": book.description
+            }
+        )
+    return jsonify(books_response)
+
+    
--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,3 +1,7 @@
+import pytest
+from app.models.book import Book
+from werkzeug.exceptions import HTTPException
+from app.routes import validate_model
 def test_get_all_books_with_no_records(client):
     response = client.get("/books")
     response_body = response.get_json()
@@ -30,3 +34,178 @@ def test_create_one_book(client):
     # Assert
     assert response.status_code == 201
     assert response_body == "Book New Book was created successfully"
+    
+def test_get_all_books_with_two_records(client, two_saved_books):
+    response = client.get("/books")
+    response_body = response.get_json()
+    # Assert
+    assert response.status_code == 200
+    assert len(response_body) == 2
+    assert response_body[0] == {
+        "id": 1,
+        "title": "Ocean Book",
+        "description": "watr 4evr"
+    }
+    assert response_body[1] == {
+        "id": 2,
+        "title": "Mountain Book",
+        "description": "i luv 2 climb rocks"
+    }
+    
+def test_get_all_books_with_title_query_matching_none(client, two_saved_books):
+    # Act
+    data = {'title': 'Desert Book'}
+    response = client.get("/books", query_string = data)
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert response_body == []
+    
+def test_get_all_books_with_title_query_matching_one(client, two_saved_books):
+    # Act
+    data = {'title': 'Ocean Book'}
+    response = client.get("/books", query_string = data)
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 200
+    assert len(response_body) == 1
+    assert response_body[0] == {
+        "id": 1,
+        "title": "Ocean Book",
+        "description": "watr 4evr"
+    }
+    
+def test_get_one_book_id_not_found(client, two_saved_books):
+    # Act
+    response = client.get("/books/3")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 404
+    assert response_body == {"message":"Book 3 not found"}
+
+def test_get_one_book_id_invalid(client, two_saved_books):
+    # Act
+    response = client.get("/books/cat")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 400
+    assert response_body == {"message":"Book cat invalid"}
+
+def test_create_one_book_no_title(client):
+    test_data = {"description": "The Best!"}
+    
+    with pytest.raises(KeyError, match='title'):
+        response = client.post("/books", json=test_data)
+    
+def test_create_one_book_no_description(client):
+    # Arrange
+    test_data = {"title": "New Book"}
+
+    # Act & Assert
+    with pytest.raises(KeyError, match = 'description'):
+        response = client.post("/books", json=test_data)
+def test_create_one_book_with_extra_keys(client, two_saved_books):
+    test_data = {
+        "extra": "some stuff",
+        "title": "New Book",
+        "description": "The Best!",
+        "another": "last value"
+    }
+    response = client.post("/books", json=test_data)
+    response_body = response.get_json()
+    
+    assert response.status_code == 201
+    assert response_body == "Book New Book was created successfully"
+    
+def test_update_book(client, two_saved_books):
+    test_data = {
+        "title": "New Book",
+        "description": "The Best!"
+    }
+    
+    response = client.put("/books/1", json=test_data)
+    response_body = response.get_json()
+    
+    assert response.status_code == 200
+    assert response_body == "Book 1 updated succesffuly"
+    
+def test_update_book_with_extra_keys(client, two_saved_books):
+    test_data = {
+        "extra": "some stuff",
+        "title": "New Book",
+        "description": "The Best!",
+        "another": "last value"
+    }
+
+    response = client.put("/books/1", json=test_data)
+    response_body = response.get_json()
+    
+    assert response.status_code == 200
+    assert response_body == "Book 1 updated succesffuly"
+    
+def test_update_book_missing_record(client, two_saved_books):
+    test_data = {
+        "title": "New Book",
+        "description": "The Best!"
+    }
+    response = client.put("/books/3", json=test_data)
+    response_body = response.get_json()
+    
+    assert response.status_code == 404
+    assert response_body == {"message" : "Book 3 not found"}
+
+def test_update_book_invalid_id(client, two_saved_books):
+    
+    test_data = {
+        "title": "New Book",
+        "description": "The Best!"
+    }
+    response = client.put("/books/cat", json=test_data)
+    response_body = response.get_json()
+    
+    assert response.status_code == 400
+    assert response_body == {"message":"Book cat invalid"}
+    
+def test_delete_book(client, two_saved_books):
+    response = client.delete("/books/1")
+    response_body = response.get_json()
+    
+    assert response.status_code == 200
+    assert response_body == "Book 1 deleted succesffuly"
+def test_delete_book_missing_record(client, two_saved_books):
+    # Act
+    response = client.delete("/books/3")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 404
+    assert response_body == {"message": "Book 3 not found"}
+
+def test_delete_book_invalid_id(client, two_saved_books):
+    # Act
+    response = client.delete("/books/cat")
+    response_body = response.get_json()
+
+    # Assert
+    assert response.status_code == 400
+    assert response_body == {"message": "Book cat invalid"}
+
+def test_validate_model(two_saved_books): 
+    result_book = validate_model(Book,1)
+    
+    assert result_book.id == 1
+    assert result_book.title == "Ocean Book"
+    assert result_book.description == "watr 4evr"
+    
+def test_validate_model_missing_record(two_saved_books):
+    with pytest.raises(HTTPException):
+        result_book = validate_model(Book, "3")
+        
+def test_validate_model_invalid_id(two_saved_books):
+    
+    with pytest.raises(HTTPException):
+        result_book = validate_model(Book, "cat")
